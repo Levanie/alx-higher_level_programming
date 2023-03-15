f9264dfba43bc6ed6d2a9099731852fdb02b2f98
Levanie <evahmugo690@gmail.com>
2023-03-15 10:33:10 -0700

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,11 @@
-#!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
-    for col in matrix:
-        new_row = [x**2 for x in col]
-        new_matrix.append(new_row)
-    return new_matrix
+    # create a new matrix with the same dimensions as the input matrix
+    result = [[0 for j in range(len(matrix[0]))] for i in range(len(matrix))]
+
+    # iterate through the input matrix and compute the square of each integer
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            result[i][j] = matrix[i][j] ** 2
+
+    # return the new matrix
+    return result
