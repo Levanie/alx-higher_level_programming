6742cef0e4a2f4de8a1bbd6688bd16ae640e1702
Levanie <evahmugo690@gmail.com>
2023-03-15 10:47:10 -0700

    2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
-        return my_list
-    else:
-        my_list[idx] = element
-        return my_list
+def uniq_add(my_list=[]):
+    new_set = set(my_list)
+    result = 0
+    for i in new_set:
+        result += i
+
+    return result
