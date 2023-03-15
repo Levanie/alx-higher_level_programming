3c97cc2b5cdbc4a789e22fa535583843f65a6161
Levanie <evahmugo690@gmail.com>
2023-03-15 10:26:30 -0700

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if matrix is not None:
-        new = []
-        for rows in matrix:
-            new.append(list(map(lambda x: x**2, rows)))
-        return (new)
-    return None
+    new_matrix = []
+    for col in matrix:
+        new_row = [x**2 for x in col]
+        new_matrix.append(new_row)
+    return new_matrix
