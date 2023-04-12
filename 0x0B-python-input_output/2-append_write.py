ad755c5871fc13977fc982f9a361417cd84ce30c
Levanie <evahmugo690@gmail.com>
2023-04-12 05:51:27 -0700

    2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,20 +1,7 @@
 #!/usr/bin/python3
-"""
-Module 2-read_lines
-Contains function that reads n lines and prints to stdout
-"""
+"""define append_write function"""
 
-
-def read_lines(filename="", nb_lines=0):
-    """reads n lines and prints to stdout
-    Print:
-        n lines: if n
-        entire file: if n is less than 1 or greater than lines in file
-    """
-    with open(filename, mode="r", encoding="utf-8") as f:
-        if nb_lines <= 0:
-            print(f.read(), end="")
-        else:
-            while nb_lines:
-                print(f.readline(), end="")
-                nb_lines -= 1
+def append_write(filename="", text=""):
+    """appends filename with utf-8"""
+    with open(filename, 'a', encoding="utf-8") as f:
+        return f.write(text)
